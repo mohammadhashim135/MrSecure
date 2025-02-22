2db925cfef2cb5c27b4387398882a703f2213644
Mohammad Hashim <hashimmohammad1375@gmail.com>
2025-02-22 18:58:35 +0530

    Updated project files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/MrSecure/", 
   plugins: [react()],
-  base: '/MrSecure/'  // Ensure it matches your repo name
-});
+})
